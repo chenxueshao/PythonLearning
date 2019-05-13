3d6116a5600cce70e50f33d25b473a4f4f0b4ddf
iwasfine <iwasfine@gmail.com>
2019-05-13 15:35:43 +0800

    Some changes

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-import http
-import math
-import sys
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(person):
-    greeting = "hello, {}".format(person)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
